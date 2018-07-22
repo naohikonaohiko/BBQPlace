63528d8780bdd1646f39f6e5a87a0aafc904f1f0
naohikonaohiko <naohikonaohiko@gmail.com>
2018-07-22 20:33:10 +0900

    add all station location

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 
 import { compose, withProps } from 'recompose';
 import {
@@ -35,13 +36,53 @@ const MyMapComponent = compose(
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
-    stations: {
-      station: {
-        name: "新浦安",
+    stations: [
+      {
+        name: "新浦安駅",
         lat: 35.632896,
         lng: 139.91256
-      }
-    },
+      },
+      {
+        name: "葛西駅",
+        lat: 35.663405,
+        lng: 139.87312
+      },
+      {
+        name: "幕張本郷駅",
+        lat: 35.672745,
+        lng: 140.042304
+      },
+      {
+        name: "両国駅",
+        lat: 35.695036,
+        lng: 139.793833
+      },
+      {
+        name: "武蔵小杉駅",
+        lat: 35.576634,
+        lng: 139.659466
+      },
+      {
+        name: "新宿駅",
+        lat: 35.689592,
+        lng: 139.700413
+      },
+      {
+        name: "流山おおたかのもり駅",
+        lat: 35.871811, 
+        lng: 139.925043
+      },
+      {
+        name: "花小金井駅",
+        lat: 35.726144,
+        lng: 139.513313
+      },
+      {
+        name: "読売ランド駅",
+        lat: 35.614768,
+        lng: 139.52815
+      },
+    ],
     circleOptions: {
       strokeColor: '#FF0000',
       strokeOpacity: 0.8,
@@ -54,12 +95,12 @@ const MyMapComponent = compose(
   withScriptjs,
   withGoogleMap
 )(props => (
-  <GoogleMap defaultZoom={11} defaultCenter={{ lat: 35.681167, lng: 139.767052 }}>
+  <GoogleMap defaultZoom={10} defaultCenter={{ lat: 35.681167, lng: 139.767052 }}>
 {/*
       {props.isMarkerShown && (
       <Marker position={{ lat: 35.681167, lng: 139.767052 }} />,
       */}
-
+{/*
       <Circle 
         center={{ lat: 35.681167, lng: 139.767052 }}
         radius={props.radius}
@@ -71,10 +112,26 @@ const MyMapComponent = compose(
         options={props.circleOptions}
       />
       <MyCircle
+        lat={props.stations.station.lat}
+        lng={props.stations.station.lng}
+        center={props.stations.station[2]}
         radius={props.radius} 
         options={props.circleOptions} 
-        />
-     <Hello />
+      />
+*/}
+      <Hello />
+     {_.map(props.stations, (stations, index) => {
+       return (
+         <Hello />,
+         <MyCircle
+           key={index}
+           lat={props.stations[index].lat}
+           lng={props.stations[index].lng}
+           radius={props.radius}
+           options={props.circleOptions}
+       />
+       );
+     })}
 {/*
     )}
   */}
@@ -93,7 +150,9 @@ class Hello extends React.Component {
 const MyCircle = (props) => {
   return (
     <Circle
-      defaultCenter={{ lat: 35.663405, lng: 139.87312 }}
+      center={{ lat: props.lat, lng: props.lng }}
+      defaultCenter={{ lat: props.lat, lng: props.lng }}
+      // defaultCenter={{ lat: 35.663405, lng: 139.87312 }}
       defaultRadius={props.radius}
       options={props.options}
     />
