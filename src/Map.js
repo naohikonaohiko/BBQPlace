8bb2e5ad92e6104891c5b70a1035aeb7339c4aa5
naohikonaohiko <naohikonaohiko@gmail.com>
2018-07-22 18:03:00 +0900

    mod circle color to blue

--- src/Map.js
+++ src/Map.js
@@ -49,7 +49,7 @@ const MyMapComponent = compose(
           strokeColor: '#FF0000',
         strokeOpacity: 0.8,
         strokeWeight: 2,
-        fillColor: '#FF0000',
+        fillColor: '#0000FF',
         fillOpacity: 0.35,
         }}
       />
